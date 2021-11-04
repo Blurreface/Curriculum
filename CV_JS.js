7dd7537ae36db0d771f34c35af3e2188bc55be7c
Blurreface <90538299+Blurreface@users.noreply.github.com>
2021-11-04 10:47:08 -0600

    Add files via upload

---
+++ CV_JS.js
@@ -0,0 +1,11 @@
+//Barra de navegacion
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+//https://codepen.io/candroo/pen/ozBEWY
